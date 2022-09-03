37653ceec813db44020f6a6d3603c0b830e3bceb
Juan Molina <juan@pcmant.com>
2022-09-03 21:41:48 +0200

    Subir archivos en NodeJS

--- aprendiendo-javascript/backend/app.js
+++ aprendiendo-javascript/backend/app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // cargar archivos rutas
+var project_routes = require('./routes/project');
 
 // middlewares
 /*
@@ -16,21 +17,17 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // CORS
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // RUTAS
-app.get('/', (req, res) => {
-	res.status(200).send('<h1>Página de inicio</h1>');
-});
-
-app.get('/test/:id', (req, res) => {
-	console.log(req.body.nombre);
-	console.log(req.query.web);
-	console.log(req.params.id);
-
-	res.status(200).send({
-		message: 'Hola mundo desde mi API de nodeJS'
-	});
-});
+app.use('/api', project_routes);
 
 // exportar
 module.exports = app;
